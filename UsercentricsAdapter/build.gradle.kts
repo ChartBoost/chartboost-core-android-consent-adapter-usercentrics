fb082671a7c336fd35058057de5ad2ca0d17f564
Helium Service <103450738+helium-service@users.noreply.github.com>
2025-07-07 10:44:41 -0700

    1.2.19.0.0 (#37)
    Co-authored-by: Ray Graham <rayg@bitwisedesign.com>

--- UsercentricsAdapter/build.gradle.kts
+++ UsercentricsAdapter/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "1.2.18.9.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "1.2.19.0.0"
 
         buildConfigField(
             "String",
@@ -85,7 +85,7 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Consent Management Platform SDK
-    implementation("com.usercentrics.sdk:usercentrics-ui:2.18.9")
+    implementation("com.usercentrics.sdk:usercentrics-ui:2.19.0")
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
